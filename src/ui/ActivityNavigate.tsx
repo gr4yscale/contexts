40bbd059da824b030d41c3be827bee47d1b343cc
Tyler Powers <gr4yscale@gmail.com>
2025-03-31 00:11:55 +0200

    cleanup unnecessary keymap config

--- src/ui/ActivityNavigate.tsx
+++ src/ui/ActivityNavigate.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Text } from "ink";
 
 import { Activity } from "../types.mts";
-
-import { key, KeymapConfig } from "./common/Keymapping.mts";
-import { KeysContext } from "./common/Context.mts";
 import { getCurrentContextActivities } from "../models/context.mts";
 import { executeAction } from "../actions.mts";
 import CoreList, { List, ListItem } from "./common/CoreList.tsx";
 
 const ActivityNavigate: React.FC = () => {
-  const [mode, setMode] = useState<ActivityNavigateStates>("find");
   const [lists, setLists] = useState<Array<List>>([]);
   const [loading, setLoading] = useState(true);
 
@@ -46,33 +42,6 @@ const ActivityNavigate: React.FC = () => {
   useEffect(() => {
     fetchActivities();
   }, []);
-
-  const { keymap }: any = useContext(KeysContext);
-
-  useEffect(() => {
-    let keymapConfig: KeymapConfig = [];
-
-    switch (mode) {
-      case "find":
-        keymapConfig = [
-          // {
-          //   sequence: [key("z")],
-          //   description: "Initial mode",
-          //   name: "set-mode-initial",
-          //   handler: () => {
-          //     setMode("initial");
-          //   },
-          // },
-        ];
-        break;
-    }
-
-    keymap.pushKeymap(keymapConfig);
-
-    return () => {
-      keymap.popKeymap();
-    };
-  }, [mode]);
 
   return (
     <Box borderStyle="single" borderColor="gray">
