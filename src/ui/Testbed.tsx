1108898971453a27abc1f32592201ab53948d02e
Tyler Powers <gr4yscale@gmail.com>
2025-06-07 16:08:09 +0200

    Use NodeSelection in Testbed

--- src/ui/Testbed.tsx
+++ src/ui/Testbed.tsx
@@ -1,45 +1,15 @@
 import { $ } from "zx";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Box } from "ink";
-import CoreList, { List } from "./common/CoreList.tsx";
-import {
-  filteredNodeTree,
-  NodeTreeFilter,
-} from "../models/node.mts";
+import NodeSelection from "./NodeSelection.tsx";
 
 const Testbed: React.FC = () => {
-  const [lists, setLists] = useState<Array<List>>([]);
-
-  const fetchNodes = async () => {
-    try {
-      const nodes = await filteredNodeTree(NodeTreeFilter.ALL);
-
-      const items = tree.map((node) => ({
-        id: node.nodeId,
-        node,
-        selected: node.selected,
-        display:
-          "  ".repeat(node.depth || 0) +
-          (node.depth || 0 > 0 ? "└─ " : "") +
-          node.name,
-      }));
-      setLists([{ items, id: "default", display: "default" }]);
-    } catch (error) {
-      console.error("Error fetching nodes:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchNodes();
-  }, []);
-
   return (
     <Box flexDirection="column" width="100%">
-      <CoreList
-        lists={lists}
+      <NodeSelection
         multiple={true}
-        onSelected={(items) => {
-          $`notify-send items: "${items}"`;
+        onSelected={(nodeIds) => {
+          $`notify-send "Selected node IDs: ${nodeIds.join(', ')}"`;
         }}
       />
     </Box>
